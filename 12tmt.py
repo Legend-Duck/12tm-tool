c50e2a2259aecc56769532b18cead1b237931a4b
Legend-Duck <legendduck666@gmail.com>
2022-09-24 22:37:18 +0800

    better format for 12tmt.py

--- 12tmt.py
+++ 12tmt.py
@@ -20,6 +20,7 @@ class Button(tk.Frame):
         self.grid(row=0, column=1, sticky=tk.NW)
         self.type = {'Auto-fill': 'auto_fill', 'Random': 'random', 'Clear': 'clear'}
         self.key = list(self.type.keys())
+
         for key in self.key:
             tk.Button(self, text=key, command=getattr(command, self.type[key])).grid(row=self.key.index(key), column=0, sticky=tk.EW)
 
@@ -30,9 +31,11 @@ class Grid(tk.Frame):
         self.columnconfigure(index=list(range(12)), weight=1)
         self.grid(row=0, column=0, sticky=tk.NSEW)
         self.entry = []
+
         for row in range(12):
             tmp = [tk.Entry(self, justify=tk.CENTER, width=3, font=('Arial',10)) for _ in range(12)]
             self.entry.append(tmp)
+
             for column in range(12):
                 tmp[column].grid(row=row, column=column, sticky=tk.NSEW)
 
@@ -44,13 +47,16 @@ class Command:
     def auto_fill(self):
         dia = self.grid_frame.entry[0][0].get()
         if not(dia.isdigit()): return
+
         for row in range(1, 12):
             self.grid_frame.entry[row][row].delete('0', 'end')
             self.grid_frame.entry[row][row].insert('0', dia)
+
         for row in range(1, 12):
             r0 = self.grid_frame.entry[0][row].get()
             if not(r0.isdigit()): continue
             diff = int(dia) - int(r0)
+
             for column in range(12):
                 r0 = self.grid_frame.entry[0][column].get()
                 if not(r0.isdigit()): continue
@@ -59,12 +65,14 @@ class Command:
 
     def random(self):
         rn.shuffle(self.num)
+
         for i in range(12):
             self.grid_frame.entry[0][i].delete('0', 'end')
             self.grid_frame.entry[0][i].insert('0', self.num[i])
         self.auto_fill()
 
     def clear(self):
+
         for row in range(12):
             for column in range(12):
                 self.grid_frame.entry[row][column].delete('0', 'end')
